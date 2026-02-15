d51c57e099057a303a8dfd8ff4017ef31d201fd9
ialikh72 <ialikh.w1@gmail.com>
2026-02-15 18:41:30 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter year:");
+        int year = int.Parse(Console.ReadLine());
+        if(year%4==0 && year%100!=0 ||(year%400!=0))
+        {
+            Console.WriteLine("year is leap....");
+        }
+        else
+            {
+            Console.WriteLine("year is not leap....");
+        }
+    }
+}
